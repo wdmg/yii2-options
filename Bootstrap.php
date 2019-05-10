be20f60931c0c70cb790df4d4dacb18def2412c5
alex-wdmg <wdmg.com.ua@gmail.com>
2019-05-10 17:14:58 +0300

    Added base Module and Bootstrap class

---
+++ Bootstrap.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace wdmg\settings;
+
+/**
+ * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
+ * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
+ * @license         https://opensource.org/licenses/MIT Massachusetts Institute of Technology (MIT) License
+ */
+
+use yii\base\BootstrapInterface;
+use Yii;
+
+
+class Bootstrap implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        // Get the module instance
+        $module = Yii::$app->getModule('settings');
+
+        // Get URL path prefix if exist
+        $prefix = (isset($module->routePrefix) ? $module->routePrefix . '/' : '');
+
+        // Add module URL rules
+        $app->getUrlManager()->addRules(
+            [
+                $prefix . '<module:settings>/' => '<module>/settings/index',
+                $prefix . '<module:settings>/<controller>/' => '<module>/<controller>',
+                $prefix . '<module:settings>/<controller>/<action>' => '<module>/<controller>/<action>',
+            ],
+            true
+        );
+    }
+}
---
+++ Module.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace wdmg\settings;
+
+/**
+ * Yii2 settings
+ *
+ * @category        Module
+ * @version         0.0.1
+ * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
+ * @link            https://github.com/wdmg/yii2-messages
+ * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
+ * @license         https://opensource.org/licenses/MIT Massachusetts Institute of Technology (MIT) License
+ *
+ */
+
+use Yii;
+
+/**
+ * Settings module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'wdmg\settings\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public $defaultRoute = 'default';
+
+    /**
+     * @var string the prefix for routing of module
+     */
+    public $routePrefix = "admin";
+
+    /**
+     * @var string the vendor name of module
+     */
+    private $vendor = "wdmg";
+
+    /**
+     * @var string the module version
+     */
+    private $version = "0.0.1";
+
+    /**
+     * @var integer, priority of initialization
+     */
+    private $priority = 3;
+
+    /**
+     * @var array of strings missing translations
+     */
+    public $missingTranslation;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // Set controller namespace for console commands
+        if (Yii::$app instanceof \yii\console\Application)
+            $this->controllerNamespace = 'wdmg\settings\commands';
+
+        // Set current version of module
+        $this->setVersion($this->version);
+
+        // Register translations
+        $this->registerTranslations();
+
+        // Normalize route prefix
+        $this->routePrefixNormalize();
+    }
+
+    /**
+     * Return module vendor
+     * @var string of current module vendor
+     */
+    public function getVendor() {
+        return $this->vendor;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function afterAction($action, $result)
+    {
+
+        // Log to debuf console missing translations
+        if (is_array($this->missingTranslation) && YII_ENV == 'dev')
+            Yii::warning('Missing translations: ' . var_export($this->missingTranslation, true), 'i18n');
+
+        $result = parent::afterAction($action, $result);
+        return $result;
+
+    }
+
+    // Registers translations for the module
+    public function registerTranslations()
+    {
+        Yii::$app->i18n->translations['app/modules/settings'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'sourceLanguage' => 'en-US',
+            'basePath' => '@vendor/wdmg/yii2-settings/messages',
+            'on missingTranslation' => function($event) {
+
+                if (YII_ENV == 'dev')
+                    $this->missingTranslation[] = $event->message;
+
+            },
+        ];
+    }
+
+    public static function t($category, $message, $params = [], $language = null)
+    {
+        return Yii::t('app/modules/settings' . $category, $message, $params, $language);
+    }
+
+    /**
+     * Normalize route prefix
+     * @return string of current route prefix
+     */
+    public function routePrefixNormalize()
+    {
+        if(!empty($this->routePrefix)) {
+            $this->routePrefix = str_replace('/', '', $this->routePrefix);
+            $this->routePrefix = '/'.$this->routePrefix;
+            $this->routePrefix = str_replace('//', '/', $this->routePrefix);
+        }
+        return $this->routePrefix;
+    }
+
+    /**
+     * Build dashboard navigation items for NavBar
+     * @return array of current module nav items
+     */
+    public function dashboardNavItems()
+    {
+        return [
+            'label' => Yii::t('app/modules/settings', 'Messages'),
+            'url' => [$this->routePrefix . '/settings/'],
+            'active' => in_array(\Yii::$app->controller->module->id, ['settings'])
+        ];
+    }
+}
