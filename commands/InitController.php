7630ffb9fa287f5d4ce9a0e168369713933dfe60
alex-wdmg <wdmg.com.ua@gmail.com>
2019-05-11 18:16:38 +0300

    Added import options from app params to console

--- commands/InitController.php
+++ commands/InitController.php
@@ -5,6 +5,7 @@ namespace wdmg\options\commands;
 use Yii;
 use yii\console\Controller;
 use yii\console\ExitCode;
+use wdmg\options\components\Options;
 
 use yii\helpers\Console;
 use yii\helpers\ArrayHelper;
@@ -18,7 +19,8 @@ class InitController extends Controller
 
     public function actionIndex($params = null)
     {
-        $version = Yii::$app->controller->module->version;
+        $module = Yii::$app->controller->module;
+        $version = $module->version;
         $welcome =
             '╔════════════════════════════════════════════════╗'. "\n" .
             '║                                                ║'. "\n" .
@@ -30,7 +32,8 @@ class InitController extends Controller
         echo $name = $this->ansiFormat($welcome . "\n\n", Console::FG_GREEN);
         echo "Select the operation you want to perform:\n";
         echo "  1) Apply all module migrations\n";
-        echo "  2) Revert all module migrations\n\n";
+        echo "  2) Revert all module migrations\n";
+        echo "  3) Scan and add all application options\n\n";
         echo "Your choice: ";
 
         $selected = trim(fgets(STDIN));
@@ -38,6 +41,11 @@ class InitController extends Controller
             Yii::$app->runAction('migrate/up', ['migrationPath' => '@vendor/wdmg/yii2-options/migrations', 'interactive' => true]);
         } else if($selected == "2") {
             Yii::$app->runAction('migrate/down', ['migrationPath' => '@vendor/wdmg/yii2-options/migrations', 'interactive' => true]);
+        } else if($selected == "3") {
+            $component = new Options;
+            foreach (Yii::$app->params as $param => $value) {
+                $component->set($param, $value, $section = null, $type = null, $label = null);
+            }
         } else {
             echo $this->ansiFormat("Error! Your selection has not been recognized.\n\n", Console::FG_RED);
             return ExitCode::UNSPECIFIED_ERROR;
